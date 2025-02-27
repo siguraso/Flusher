d7dbe86d5efde8e838ec20f387cc3478c1bc18a6
sigurdandris <sigurd.andris.sorengen@gmail.com>
2025-02-27 15:34:38 +0100

    added DeckOfCards class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module siguraso.flusher.flusher {
   requires javafx.controls;
   requires javafx.graphics;
 
-  opens siguraso.flusher.flusher to javafx.graphics;
-  exports siguraso.flusher.flusher;
-  exports siguraso.flusher;
   opens siguraso.flusher to javafx.graphics;
+  exports siguraso.flusher;
 }
---
+++ src/main/java/siguraso/flusher/model/DeckOfCards.java
@@ -0,0 +1,52 @@
+package siguraso.flusher.model;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class DeckOfCards {
+
+  // suits in unicode characters
+  private final char[] suits = {'♥', '♦', '♣', '♠'};
+
+  ArrayList<String> cards = new ArrayList<>();
+
+  public DeckOfCards() {
+    IntStream.range(1, 53).boxed().forEach(i -> {
+      for (char suit : suits) {
+        cards.add(String.valueOf(suit) + i);
+      }
+    });
+  }
+
+  public ArrayList<String> getDeckOfCards() {
+    return cards;
+  }
+
+  public String[] dealHand(int cards) {
+    String[] hand = new String[cards];
+    Random rand = new Random();
+    // create a temporary list to remove cards from
+    ArrayList<String> tempCards = this.cards;
+
+    for (int i = 0; i < cards; i++) {
+      int cardIndex = rand.nextInt(tempCards.size());
+      hand[i] = tempCards.get(cardIndex);
+      tempCards.remove(cardIndex);
+    }
+
+    return hand;
+  }
+
+  public boolean checkFlush(String[] hand) {
+    char suit = hand[0].charAt(0);
+    for (String card : hand) {
+      if (card.charAt(0) != suit) {
+        return false;
+      }
+    }
+    
+    return true;
+  }
+
+}
